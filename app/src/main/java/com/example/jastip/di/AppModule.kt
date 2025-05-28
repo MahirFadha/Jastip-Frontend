efd01037ff9bebc315f4fcd2b2b507180d05c652
Mahir Fadha <mahirfadha@gmail.com>
2025-05-28 10:56:59 +0700

    Scrapping

--- app/src/main/java/com/example/jastip/di/AppModule.kt
+++ app/src/main/java/com/example/jastip/di/AppModule.kt
@@ -19,7 +19,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 val MIGRATION_2_3 = object : Migration(2, 3) {
     override fun migrate(database: SupportSQLiteDatabase) {
